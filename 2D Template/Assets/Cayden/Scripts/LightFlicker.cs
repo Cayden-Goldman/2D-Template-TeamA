8f9afe2dae9893fa342fec414981db985b8da981
MUKILTEO\1717590 <1717590@mukilteo.wednet.edu>
2024-12-03 09:29:04 -0800

    particles

--- 2D Template/Assets/Cayden/Scripts/LightFlicker.cs
+++ 2D Template/Assets/Cayden/Scripts/LightFlicker.cs
@@ -7,19 +7,22 @@ using UnityEngine.Rendering.Universal;
 public class LightFlicker : MonoBehaviour
 {
     Light2D light;
-    float lightness = 0.9f;
+    float lightness;
     float green = 0.3f;
+    float startRad;
 
     void Start()
     {
         light = GetComponent<Light2D>();
+        startRad = light.pointLightOuterRadius;
+        lightness = startRad - 0.1f;
     }
 
     void Update()
     {
-        lightness = Mathf.Clamp(lightness + Time.deltaTime * 3 * Random.Range(-1, 1f), 0.9f, 1);
-        light.pointLightOuterRadius = lightness * 3;
+        lightness = Mathf.Clamp(lightness + Time.deltaTime * 3 * Random.Range(-1, 1f), startRad - 0.1f, startRad);
+        light.pointLightOuterRadius = lightness;
         green = Mathf.Clamp(green + Time.deltaTime * 2 * Random.Range(-1, 1f), 0.2f, 0.35f);
-        light.color = new Color(1, green, 0) * lightness;
+        light.color = new Color(1, green, 0);
     }
 }
