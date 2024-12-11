9f1d24b37df54100fa7969594871d500eb3d0c71
MUKILTEO\1717295 <1717295@mukilteo.wednet.edu>
2024-12-11 09:30:52 -0800

    abcdefghijklmnop

--- 2D Template/Assets/Jaden/Scripts/CarScript.cs
+++ 2D Template/Assets/Jaden/Scripts/CarScript.cs
@@ -1,26 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.EventSystems;
 
-public class CarScript : MonoBehaviour, IPointerEnterHandler
+public class CarScript : MonoBehaviour
 {
     public Vector3 mouse;
-    public GameObject[] gameObjects;
+    public GameObject ooooo;
+    public List<Vector3> ooooo2;
     public bool vertical;
+    public void Start()
+    {
+        for(int i = 0; i < ooooo.transform.childCount; i++)
+            ooooo2.Add(ooooo.transform.GetChild(i).position);
+    }
     public void Update()
     {
+        
         mouse = new Vector3(Input.mousePosition.x, Input.mousePosition.y, .1f);
     }
     public void OnMouseDrag()
     {
-        if(vertical)
-        transform.position = new Vector3(transform.position.x, Mathf.Round(Camera.main.ScreenToWorldPoint(mouse).y + .5f) - .5f, transform.position.z);
+
+        if (vertical)
+        {
+            
+        }
         else
         transform.position = new Vector3(Mathf.Round(Camera.main.ScreenToWorldPoint(mouse).x + .5f) - .5f , transform.position.y, transform.position.z);
-    }
-    public void OnPointerEnter(PointerEventData eventData)
-    {
-        Debug.Log("The cursor entered the selectable UI element.");
     }
 }
