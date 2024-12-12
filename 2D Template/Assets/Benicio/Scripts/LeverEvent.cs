f4c7fc05b3635fdd9fef3ad3814ebdc54f0c9eef
MUKILTEO\1717590 <1717590@mukilteo.wednet.edu>
2024-12-12 10:19:00 -0800

    slugma

--- 2D Template/Assets/Benicio/Scripts/LeverEvent.cs
+++ 2D Template/Assets/Benicio/Scripts/LeverEvent.cs
@@ -7,32 +7,13 @@ public class LeverEvent : MonoBehaviour
 {
     public static UnityEvent evennt = new();
     public GameObject door;
-    // Start is called before the first frame update
+    bool aktiv = true;
     void Start()
     {
         evennt.AddListener(OpenDoor);
     }
-
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
-
     public void OpenDoor()
     {
-        bool aktiv = true;
-        if (aktiv)
-        {
-            door.SetActive(false);
-            aktiv = false;
-        }
-        else
-        {
-            door.SetActive(true);
-            aktiv = true;
-        }
+            door.SetActive(aktiv = !aktiv);
     }
-
-
 }
