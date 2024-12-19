0c34ded7700dbe1365ce443b7df08fae8e673231
MUKILTEO\1717590 <1717590@mukilteo.wednet.edu>
2024-12-19 08:58:19 -0800

    git commit -m "totally gumpular bro"

--- 2D Template/Assets/Benicio/Scripts/LeverEvent.cs
+++ 2D Template/Assets/Benicio/Scripts/LeverEvent.cs
@@ -2,18 +2,29 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
+using UnityEngine.Tilemaps;
 
 public class LeverEvent : MonoBehaviour
 {
     public static UnityEvent evennt = new();
+    bool aktiv = false;
     public GameObject door;
-    bool aktiv = true;
+    public Vector2Int[] tilePositions;
+    public TileBase invisibleTile;
+
+    Tilemap walls;
     void Start()
     {
         evennt.AddListener(OpenDoor);
+        walls = GameObject.Find("Collidables").GetComponent<Tilemap>();
+        foreach (Vector2Int pos in tilePositions)
+            walls.SetTile((Vector3Int)pos, invisibleTile);
     }
     public void OpenDoor()
     {
-            door.SetActive(aktiv = !aktiv);
+        if(aktiv)
+            ButtonPress.Door(door, walls, tilePositions, invisibleTile, aktiv = !aktiv);
+        else
+            ButtonPress.Door(door, walls, tilePositions, null, aktiv = !aktiv);
     }
 }
--- 2D Template/Assets/Cayden/Scripts/ButtonPress.cs
+++ 2D Template/Assets/Cayden/Scripts/ButtonPress.cs
@@ -1,19 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class ButtonPress : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public GameObject door;
+    public Vector2Int[] tilePositions;
+    public TileBase invisibleTile;
+    
+    Tilemap walls;
+
     void Start()
     {
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        walls = GameObject.Find("Collidables").GetComponent<Tilemap>();
+        foreach (Vector2Int pos in tilePositions)
+            walls.SetTile((Vector3Int)pos, invisibleTile);
     }
 
     public void OnTriggerEnter2D(Collider2D collision)
@@ -21,6 +23,7 @@ public class ButtonPress : MonoBehaviour
         if(collision.gameObject.CompareTag("Player") || collision.gameObject.CompareTag("Box"))
         {
             GetComponent<SpriteRenderer>().color = Color.green;
+            Door(door, walls, tilePositions, null, true);
         }
     }
     public void OnTriggerExit2D(Collider2D collision)
@@ -28,6 +31,14 @@ public class ButtonPress : MonoBehaviour
         if (collision.gameObject.CompareTag("Player") || collision.gameObject.CompareTag("Box"))
         {
             GetComponent<SpriteRenderer>().color = Color.red;
+            Door(door, walls, tilePositions, invisibleTile, false);
         }
+    }
+
+    public static void Door(GameObject door, Tilemap tilemap, Vector2Int[] tiles, TileBase tile, bool open)
+    {
+        door.SetActive(!open);
+        foreach (Vector2Int pos in tiles)
+            tilemap.SetTile((Vector3Int)pos, tile);
     }
 }
