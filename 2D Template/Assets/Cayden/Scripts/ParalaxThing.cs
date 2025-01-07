7f1d8d4fbb52d34db2485cecd84d10a61496856f
MUKILTEO\1717590 <1717590@mukilteo.wednet.edu>
2025-01-07 10:16:17 -0800

    stuppod

---
+++ 2D Template/Assets/Cayden/Scripts/ParalaxThing.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParalaxThing : MonoBehaviour
+{
+    public GameObject player;
+    Vector3 ogCamPos;
+    Vector3 position;
+    private void Start()
+    {
+        ogCamPos = transform.position;
+    }
+    void Update()
+    {
+        if(player.transform.position.y < ogCamPos.y)
+            position.y = ogCamPos.y - 1;
+        else if(player.transform.position.y > ogCamPos.y)
+            position.y = ogCamPos.y + 3;
+        transform.position = position;
+    }
+}
