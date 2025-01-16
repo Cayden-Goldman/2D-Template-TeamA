da33169592b83f9639249c474e9356f0e9bf2bbc
Cayden Goldman <caydengoldman@outlook.com>
2025-01-15 16:07:17 -0800

    small number change

--- 2D Template/Assets/Cayden/Scripts/ParalaxThing.cs
+++ 2D Template/Assets/Cayden/Scripts/ParalaxThing.cs
@@ -18,13 +18,13 @@ public class ParalaxThing : MonoBehaviour
             ghost = GameObject.Find("Ghost(Clone)");
         else
             ghost = player;
-        if(ghost.transform.position.y < ogCamPos.y)
+        if(ghost.transform.position.y < ogCamPos.y )
             position.y = ogCamPos.y - 1;
         else if(ghost.transform.position.y > ogCamPos.y)
             position.y = ogCamPos.y + 3;
-        if (ghost.transform.position.x < ogCamPos.x)
+        if (ghost.transform.position.x < ogCamPos.x - 0.5f)
             position.x = ogCamPos.x - 1;
-        else if (ghost.transform.position.x > ogCamPos.x)
+        else if (ghost.transform.position.x > ogCamPos.x - 0.5f)
             position.x = ogCamPos.x + 1;
         transform.position = new Vector3(position.x, position.y, transform.position.z);
     }
