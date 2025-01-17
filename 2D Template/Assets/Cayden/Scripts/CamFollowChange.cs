74b01e79e837b823e8c37017225aaec73e5e6e35
Cayden Goldman <caydengoldman@outlook.com>
2025-01-17 14:29:35 -0800

    chanfgegs

--- 2D Template/Assets/Cayden/Scripts/CamFollowChange.cs
+++ 2D Template/Assets/Cayden/Scripts/CamFollowChange.cs
@@ -8,7 +8,7 @@ public class CamFollowChange : MonoBehaviour
     public GameObject followThing, camera;
     public void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.CompareTag("Player"))
+        if (collision.gameObject.CompareTag("Player") || collision.gameObject.CompareTag("Ghost"))
         {
             camera.GetComponent<CinemachineVirtualCamera>().Follow = followThing.transform;
         }
