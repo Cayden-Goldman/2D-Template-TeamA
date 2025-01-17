1a2d27d41bdb0792f7a3cbc43c42cb0189debfd1
Cayden Goldman <caydengoldman@outlook.com>
2025-01-17 15:14:23 -0800

    srufyhjk

---
+++ 2D Template/Assets/Benicio/Scripts/BossThingy.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BossThingy : MonoBehaviour
+{
+    public bool onBoss;
+    public GameObject ghost;
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        ghost = collision.gameObject;
+        if (collision.gameObject.CompareTag("Ghost"))
+            onBoss = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (onBoss && Input.GetKeyDown(KeyCode.Space))
+        {
+            ghost.SetActive(false);
+            SceneManager.LoadScene("Title Scene");
+        }
+    }
+}
